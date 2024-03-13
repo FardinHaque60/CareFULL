8edd29a6c2622eabe756af20c176cb1de3663d4e
Fardin Haque <fardinhaque60@gmail.com>
2024-03-13 13:06:16 -0700

    fixed backend-frontend routing

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function LandingPage() {
+  const [message, setMessage] = useState('');
+
+  {/* we can go to this link to see debugging info for backend */}
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/landing-page/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="text-3xl font-bold underline">
-      Hello world!
+    <div className="text-3xl font-bold">
+      <h1> Welcome to CareFULL </h1>
+      <p> {message} </p> {/* this message is taken from backend */}
     </div>
   );
 }
