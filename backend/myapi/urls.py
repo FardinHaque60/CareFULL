ffa468af5942d739fc0479d12ad52b4e0ace0ff3
Fardin Haque <fardinhaque60@gmail.com>
2024-04-11 11:44:58 -0700

    added logout endpoint

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('landing-page/',views.landing_page, name='landing-page'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('create-account/', views.create_account, name='create-account'),
     path('chatbot/', views.chatbot, name='chatbot'),
 ]
