a30ae99e447c4b8f820bc9d53904a2f6316da9e1
Fardin Haque <fardinhaque60@gmail.com>
2024-04-12 18:36:31 -0700

    registered appointment obj

--- backend/myapi/models.py
+++ backend/myapi/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 # test model to see if db is working
-class test(models.Model):
-    name = models.CharField(max_length=100)
+class Appointment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.CharField(max_length=8)
+    description = models.CharField()
     def __str__(self):
-        return self.name
+        return self.title
