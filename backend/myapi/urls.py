bea14f2db2e52271caab29448e007b26d3b37040
Fardin Haque <fardinhaque60@gmail.com>
2024-04-12 18:36:55 -0700

    logged backend endpoints

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from . import views
+from .views import appointments, authentication, chatbot, create_account, landing_page
 
 urlpatterns = [
-    path('landing-page/',views.landing_page, name='landing-page'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('create-account/', views.create_account, name='create-account'),
-    path('chatbot/', views.chatbot, name='chatbot'),
+#landing page endpoints:
+    path('get-user-info/', landing_page.get_user_info, name='get-user-info'),
+#authentication endpoints:
+    path('login/', authentication.login, name='login'),
+    path('get-user-status/', authentication.get_user_status, name="get-user-status"),
+    path('logout/', authentication.logout, name='logout'),
+#create account endpoint:
+    path('create-account/', create_account.create_account, name='create-account'),
+#chatbot endpoints:
+    #path('get-message/', chatbot.get_message, name='get-message'),
+    #path('load-history', chatbot.load_history, name='load-history'),
+#appointments endpoints:
+    path('save-appointment/', appointments.save_appointment, name='save-appointment'),
+    path('get-appointments/', appointments.get_appointments, name='get-appointments'),
 ]
