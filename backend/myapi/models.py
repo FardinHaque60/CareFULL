8f420718f31fc65d4e1833b69c45bc7a0adefa17
Fardin Haque <fardinhaque60@gmail.com>
2024-04-21 09:57:33 -0700

    added messages object

--- backend/myapi/models.py
+++ backend/myapi/models.py
@@ -1,13 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 
 # test model to see if db is working
 class Appointment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     date = models.DateField()
-    time = models.CharField(max_length=8)
+    time = models.TimeField()
     description = models.CharField()
     def __str__(self):
         return self.title
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField()
+    date = models.DateField()
+    time = models.TimeField()
+    response = models.BooleanField(default=False)
+    prompt = models.BooleanField(default=False)
+    def __str__(self):
+        return self.body
